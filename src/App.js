f90f5ef569bfb635ba4c7b244eac1d6ed37789d0
Ronit Aryan <ronitaryan@MacBook-Pro.local>
2020-04-11 00:06:42 +0530

    Completed the shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import { Route, Switch } from 'react-router-dom';
 
-const DenimsPage = () => (
-  <div>
-    <h1>Denims Page</h1>
-  </div>
-);
 
 function App() { 
    return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/denims' component={DenimsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
