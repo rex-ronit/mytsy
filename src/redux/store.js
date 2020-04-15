a347afd9badcf1b7e83d459f9ade0906fc35ffce
Ronit Aryan <ronitaryan@MacBook-Pro.local>
2020-04-15 14:09:28 +0530

    implemented firebase utils

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import rootReducer from "./root-reducer";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+
+const middlewares = [logger];
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
