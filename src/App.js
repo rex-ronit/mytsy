73af84af4d318a132be2c845e11d4bf747b9b57e
Ronit Aryan <ronit@Ronits-MacBook-Pro.local>
2020-06-30 15:44:26 +0530

    checkout page done

--- src/App.js
+++ src/App.js
@@ -1,66 +1,68 @@
-import React from 'react';
-import './App.scss';
-import HomePage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop';
-import Header from './components/header/header';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import SignInUp from './pages/sign-in-up/sign-in-up';
-import CheckoutPage from './pages/checkout/checkout';
-import { auth, createUserProfileDoc } from './firebase/firebase.utils';
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import "./App.scss";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
+import { Route, Switch, Redirect } from "react-router-dom";
+import SignInUp from "./pages/sign-in-up/sign-in-up";
+import CheckoutPage from "./pages/checkout/checkout";
+import { auth, createUserProfileDoc } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
-	unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null;
 
-	componentDidMount() {
-		const { setCurrentUser } = this.props;
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-			if (userAuth) {
-				const userRef = await createUserProfileDoc(userAuth);
-				userRef.onSnapshot((snapshop) => {
-					setCurrentUser({
-						id: snapshop.id,
-						...snapshop.data()
-					});
-				});
-			} else {
-				setCurrentUser(userAuth);
-			}
-		});
-	}
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDoc(userAuth);
+        userRef.onSnapshot((snapshop) => {
+          setCurrentUser({
+            id: snapshop.id,
+            ...snapshop.data(),
+          });
+        });
+      } else {
+        setCurrentUser(userAuth);
+      }
+    });
+  }
 
-	componentWillUnmount() {
-		this.unsubscribeFromAuth();
-	}
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-	render() {
-		return (
-			<div>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/shop" component={ShopPage} />
-					<Route exact path="/checkout" component={CheckoutPage} />
-					<Route
-						exact
-						path="/signin"
-						render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInUp />)}
-					/>
-				</Switch>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInUp />
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-	currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	setCurrentUser: (user) => dispatch(setCurrentUser(user))
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -6,23 +6,32 @@ import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
+import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {
-        cartItems.length ?
-      cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      )) : <span className="empty">Your cart is empty</span>
-      }
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty">Your cart is empty</span>
+      )}
     </div>
-    <Button onClick={() => history.push('/checkout')}>GO TO CHECKOUT</Button>
+    <Button
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
+    </Button>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems 
+  cartItems: selectCartItems,
 });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
